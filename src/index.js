d8b5a2aa0e4885f8371d39d536bf091a2ad0222f
IvanKahinga <ikahinga@gmail.com>
2022-12-13 12:48:26 +0300

    Completed tasklister assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const inputBox = document.getElementsByTagName('input')[1]
+  inputBox.addEventListener("click", boxClick, false)
+  function boxClick (event) {
+    event.preventDefault()    
+
+  }
 });
+console.log(inputBox);
